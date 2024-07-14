b772b1217c594ab261bb5133875cfb3553fa4ede
Matthew Belanger <matt_belanger1@outlook.com>
2024-07-14 19:24:30 -0400

    fixed typo

--- modules/board/src/piece.cpp
+++ modules/board/src/piece.cpp
@@ -2,7 +2,7 @@
 
 namespace chess::board {
 
-    piece_t::piece_t( piece_type type, colour_t colour ) : type( type ), colour( colour ), has_moved( false ) {}
+    piece_t::piece_t( piece_type type, colour_t colour ) : type( type ), colour( colour ), piece_has_moved( false ) {}
     bool piece_t::has_moved() { return piece_has_moved; }
 
 }  // namespace chess::board
