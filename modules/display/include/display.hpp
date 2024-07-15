a7449cef5479d22f7c4933fc46b3f83c0a8a9511
Matthew Belanger <matt_belanger1@outlook.com>
2024-07-14 21:01:42 -0400

    included imgui using conan

---
+++ modules/display/include/display.hpp
@@ -0,0 +1,18 @@
+#ifndef __CHESS__DISPLAY__DISPLAY__HPP__
+#define __CHESS__DISPLAY__DISPLAY__HPP__
+
+#include <imgui.h>
+
+#include <string>
+
+namespace chess::display {
+
+    class display {
+    public:
+    private:
+        std::string window_name;
+    };
+
+}  // namespace chess::display
+
+#endif
